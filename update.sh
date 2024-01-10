68dddebe0fd42c973909acb4684737ec015e2c05
[DEV_SANJAY] <144456077+WHITEDH4CKER@users.noreply.github.com>
2024-01-10 12:14:20 +0530

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Update Script
+# Script created by @WHITEDH4CKER
+
+
+dependencies() {
+
+command -v git > /dev/null 2>&1 || { echo >&2 "Package GIT is not installed ... Unable to update ..."; exit 1; }
+
+}
+
+script() {
+
+clear
+printf "\n \e[1;92mUpdating \e[1;94mShellPhish\e[1;92m directory ...\n\n"
+sleep 1.5
+cd ..
+rm -rf IG-BRUTEFORCE
+git clone https://github.com/WHITEDH4CKER/IG-BRUTEFORCE
+cd IG-BRUTEFOTCE
+printf "\n\e[1;92m Update Complete ...\n\e[0m"
+
+}
+
+dependencies
+script
